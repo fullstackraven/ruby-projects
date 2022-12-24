5e82a12d1769ca6388b96881f57e1e18283323ae
fullstackraven <fullstackraven@gmail.com>
2022-12-23 23:37:35 -0500

    Add cipher functionality

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,4 +1,21 @@
-#Caesar cipher that takes initial string and outputs modified strong per cipher shift factor
+#Caesar Cipher that takes initial string and outputs modified string per cipher shift factor
 
-class CaesarCipher
-    
+def caesar_cipher(string, shift, result = '')
+  string.each_char do |char|
+    codepoint = char.ord < 91 ? 65 : 97
+    # Handles ASCII code to charater conversion for lowercase & uppercase 
+    if char.ord.between?(65, 90) || char.ord.between?(97,122)
+      encrypter = (((char.ord - codepoint) + shift) % 26) + codepoint
+      result += encrypter.chr
+    # Handles string destruction and reconstruction, as well as, punctuation & spaces
+    else
+      result += char
+    end
+  end
+  if shift == 26
+    puts "Encryption not completed: " + string
+  # Handles encryption error message based on shift input that wraps to original string
+  else
+    result
+  end
+end
